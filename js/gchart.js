a10570c40df80a40e1bf490dcd39f56d24fe42a3
greentea524 <cpdawei@gmail.com>
2022-11-20 23:45:47 -0800

    Updates

---
+++ js/gchart.js
@@ -0,0 +1,20 @@
+google.charts.load("current", {packages:["corechart"]});
+//google.charts.setOnLoadCallback(drawChart);
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work',     8],
+    ['Eat',      2],
+    ['Commute',  1],
+    ['Entertainment', 6],
+    ['Sleep',    7]
+  ]);
+
+  var options = {
+    title: 'My Daily Activities',
+    pieHole: 0.4,
+  };
+
+  var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
+  chart.draw(data, options);
+}
