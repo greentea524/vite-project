2bb8f8f0b1bb828342996fca973f652fd4da71da
greentea524 <cpdawei@gmail.com>
2022-11-21 21:35:39 -0800

    css

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import ReactTabHeader from './component/ReactTabHeader.jsx';
 function App() {
 
   return (
-
     <div className="App">
+      <div className="navbrand">{"<David Phong />"}</div>
       <ReactTabHeader />
     </div>
   )
