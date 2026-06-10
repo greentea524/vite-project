65aecc357dfe264c0af970c411a1e69587f9cd94
greentea524 <cpdawei@gmail.com>
2026-06-10 15:56:00 -0700

    test

--- src/component/ReactTabHeader.jsx
+++ src/component/ReactTabHeader.jsx
@@ -73,6 +73,14 @@ class ReactTabHeader extends Component {
                 <i className="fa fa-gamepad fa-2x"></i> Invasion
               </a>
             </div>
+            <div className="cta text-center">
+              <a
+                className="my-button cta-button"
+                href="https://greentea524.github.io/game/js-pacman/"
+              >
+                <i className="fa fa-gamepad fa-2x"></i> Pacman
+              </a>
+            </div>
           </div>
         </Tab>
         <Tab eventKey="minesweeper" title="Minesweeper">
